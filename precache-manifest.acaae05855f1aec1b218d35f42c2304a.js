2c5756fa8fce3cd2088312538b994b19de5345f9
Mrrwmix <seidoc@gmail.com>
2019-09-09 23:49:22 -0700

    Updates

---
+++ precache-manifest.acaae05855f1aec1b218d35f42c2304a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b228d86ca4f599a541ed007da6d1b501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d95c17b0018613d72d5",
+    "url": "/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "ea5dcfe79d1d102c92d3",
+    "url": "/static/js/2.43a7f072.chunk.js"
+  },
+  {
+    "revision": "6d95c17b0018613d72d5",
+    "url": "/static/js/main.5c73755a.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  }
+]);
